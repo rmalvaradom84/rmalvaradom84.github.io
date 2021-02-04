d193bebd6410bcb3da44239675e7d7afee02e6c1
Rogelio Alvarado <71091065+rmalvaradom84@users.noreply.github.com>
2021-02-03 19:01:47 -0500

    P01

---
+++ notas.R
@@ -0,0 +1,14 @@
+#rmarkdown::render_site(encoding = "UTF-8")
+#pagina web con r
+#tutorial https://www.youtube.com/watch?v=JxsreeFsu8g
+#https://bookdown.org/yihui/rmarkdown/rmarkdown-site.html
+#https://bookdown.org/yihui/rmarkdown/html-document.html
+
+#git add .
+#git add -A
+#git commit -m "primera prueba"
+#git push origin master
+
+#install.packages("esquisse")
+#data("iris")
+#esquisse::esquisser()
